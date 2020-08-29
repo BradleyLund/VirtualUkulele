e36ee77239b58ec41cc6d469ac2be291eab78f07
Bradley Lund <bradjlund@gmail.com>
2020-08-29 13:59:33 +0200

    noselect text

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var divs = document.querySelectorAll('.child').forEach(div=>{
+    div.classList.add('noselect')
     
     div.addEventListener('click',event => {
         const audio = document.querySelector(`audio[data-key="${div.attributes[1].value}"]`)
         audio.currentTime =0
         audio.play()
     })
+
+
 })
 
 document.addEventListener('keydown',function(e) {

0ee79c00448ea4619d598861c0ff0335488d1fd1
Bradley Lund <bradjlund@gmail.com>
2020-08-29 13:54:27 +0200

    add onclick for each div

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+var divs = document.querySelectorAll('.child').forEach(div=>{
+    
+    div.addEventListener('click',event => {
+        const audio = document.querySelector(`audio[data-key="${div.attributes[1].value}"]`)
+        audio.currentTime =0
+        audio.play()
+    })
+})
+
 document.addEventListener('keydown',function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     audio.currentTime =0
